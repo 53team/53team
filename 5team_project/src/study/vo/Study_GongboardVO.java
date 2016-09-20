3602164304d28b6ddab1241f875194290d0f8230
repl1224 <jaywalk1224@gmail.com>
2016-09-20 11:14:56 +0900

    vo

---
+++ 5team_project/src/study/vo/Study_GongboardVO.java
@@ -0,0 +1,47 @@
+package study.vo;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Study_GongboardVO implements Serializable {
+	private int num;
+	private String writer, subject, content, readnum;
+	private Timestamp reg_date;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public String getReadnum() {
+		return readnum;
+	}
+	public void setReadnum(String readnum) {
+		this.readnum = readnum;
+	}
+	public Timestamp getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
+	}
+}
---
+++ 5team_project/src/study/vo/Study_ReplyboardVO.java
@@ -0,0 +1,41 @@
+package study.vo;
+
+import java.io.Serializable;
+import java.sql.Timestamp;
+
+public class Study_ReplyboardVO implements Serializable {
+	private int num, reply_num;
+	private String writer, content;
+	private Timestamp reg_date;
+	
+	public int getNum() {
+		return num;
+	}
+	public void setNum(int num) {
+		this.num = num;
+	}
+	public int getReply_num() {
+		return reply_num;
+	}
+	public void setReply_num(int reply_num) {
+		this.reply_num = reply_num;
+	}
+	public String getWriter() {
+		return writer;
+	}
+	public void setWriter(String writer) {
+		this.writer = writer;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	public Timestamp getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(Timestamp reg_date) {
+		this.reg_date = reg_date;
+	}
+}
